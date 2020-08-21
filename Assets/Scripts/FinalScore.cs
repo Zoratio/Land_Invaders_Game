ac30561850b4b1d6b18f5436fd99109e0366c143
Taylor Bromley <taylorbromley@sky.com>
2020-08-21 19:53:25 +0100

    Completed v2

---
+++ Assets/Scripts/FinalScore.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class FinalScore : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Text txt = GetComponent<Text>();
+        txt.text = PlayerPrefs.GetInt("FinalScore").ToString();
+        PlayerPrefs.SetInt("FinalScore", 0);
+    }
+
+    public void PlayAgain()
+    {
+        SceneManager.LoadScene(0);
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
---
+++ Assets/Scripts/LevelFinish.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelFinish : MonoBehaviour
+{
+    private void Start()
+    {
+        Invoke("NextScene", 68);
+    }
+
+    private void NextScene()
+    {
+        GameObject go = GameObject.Find("Score Board");
+        ScoreBoard sb = go.GetComponent<ScoreBoard>();
+        PlayerPrefs.SetInt("FinalScore", sb.score);
+        SceneManager.LoadScene(2);
+    }
+}
