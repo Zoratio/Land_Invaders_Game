5399e095952d155abcf3c8ebf3f37dadd868cb04
Taylor Bromley <taylorbromley@sky.com>
2021-03-03 12:08:58 +0000

    Small changes to end scene

---
+++ Assets/Scripts/EndButtons.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class EndButtons : MonoBehaviour
+{
+
+    public void PlayAgain()
+    {
+        SceneManager.LoadScene(0);
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
--- Assets/Scripts/FinalScore.cs
+++ Assets/Scripts/FinalScore.cs
@@ -2,25 +2,16 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
-using UnityEngine.SceneManagement;
 
 public class FinalScore : MonoBehaviour
 {
+    Text txt;
     // Start is called before the first frame update
     void Start()
     {
-        Text txt = GetComponent<Text>();
+        txt = GetComponent<Text>();
         txt.text = PlayerPrefs.GetInt("FinalScore").ToString();
         PlayerPrefs.SetInt("FinalScore", 0);
     }
 
-    public void PlayAgain()
-    {
-        SceneManager.LoadScene(0);
-    }
-
-    public void QuitGame()
-    {
-        Application.Quit();
-    }
 }
